385036231a56dd345e221f8b333e9c26f910fefe
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-09-18 21:30:36 +0500

    Online Shooping

--- pages/views.py
+++ pages/views.py
@@ -4,27 +4,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import ad, salomatlik, kitoblar, quron, offis, ibodat, sovga, parfyum, gozallik
 # Create your views here.
 
-
-class Home(ListView):
-    model = ad
-    template_name = 'pages/index.html'
-    context_object_name = 'ad'
-
-
-class Detail(DetailView):
-    model = ad
-    template_name = 'pages/detail.html'
-    context_object_name = 'ad'
-
-
-class Post(LoginRequiredMixin, CreateView):
-    model = ad
-    fields = ['product_img', 'product_name', 'description', 'price', 'location', 'phone_number']
-    template_name = 'pages/posts.html'
-    success_url = '/'
-    login_url = 'login'
-
-
 # Categories
 class Salomatlik(ListView):
     model = salomatlik
