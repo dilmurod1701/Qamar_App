c7b32cc0e2e3a0c05e945d6df8ad09e812035b55
Sarvar <118446481+OmonovSarvar@users.noreply.github.com>
2023-09-24 23:34:43 +0500

    Update views.py
    Updated users/views.py

--- users/views.py
+++ users/views.py
@@ -16,30 +16,8 @@ class Login(LoginView):
 
 
 class Logout(LogoutView):
-    next_page = 'login'
-
-
-def login_user(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.success(request, ("There Was An Error Logging In, Try Again..."))
-            return redirect('login')
-
-
-    else:
-        return render(request, 'users/login.html', {})
-
-
-def logout_user(request):
-    logout(request)
-    messages.success(request, ("You Were Logged Out!"))
-    return redirect('home')
+    template_name = 'users/logout.html'
+    next_page = '/'
 
 
 def register_user(request):
