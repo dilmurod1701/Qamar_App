a926dcecad03e54d424425c6e3389ddc47458c6d
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-09-25 00:35:00 +0500

    Update admin.py and Deleted unnecessary models

--- pages/admin.py
+++ pages/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 
-from .models import ad , salomatlik, kitoblar, quron, offis, ibodat, sovga, parfyum, gozallik
+from .models import Category, products
 
 # Register your models here.
-admin.site.register(ad)
-admin.site.register(salomatlik)
-admin.site.register(kitoblar)
-admin.site.register(quron)
-admin.site.register(offis)
-admin.site.register(ibodat)
-admin.site.register(sovga)
-admin.site.register(parfyum)
-admin.site.register(gozallik)
+admin.site.register(Category)
+admin.site.register(products)
