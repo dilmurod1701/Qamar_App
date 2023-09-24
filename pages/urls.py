66583ef7f0b1e2565c8fa09f10a682d647b7d88a
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-09-25 00:36:27 +0500

    Update urls.py and deleted unnecessary urls

--- pages/urls.py
+++ pages/urls.py
@@ -4,18 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('ad/<int:pk>', views.Detail.as_view(), name='detail'),
+    path('products/<int:pk>', views.Detail.as_view(), name='detail'),
     path('new/', views.Post.as_view(), name='post'),
+    path('category/<str:cats>', views.CategoryView, name='category'),
     path('search/', views.SearchViews, name='search-item'),
-    path('ad/search/', views.SearchViews, name='search-item'),
-    path('new/search/', views.SearchViews, name='search-item'),
-    path('salomatlik/', views.Salomatlik.as_view(), name='salomatlik'),
-    path('kitoblar/', views.Kitoblar.as_view(), name='kitoblar'),
-    path('quron/', views.Quron.as_view(), name='quron'),
-    path('uyvaoffis/', views.UyOffis.as_view(), name='uyoffis'),
-    path('ibodat/', views.Ibodat.as_view(), name='ibodat'),
-    path('sovga/', views.Sovga.as_view(), name='sovga'),
-    path('parfyum/', views.Parfyum.as_view(), name='parfyum'),
-    path('gozallik/', views.Gozallik.as_view(), name='gozallik'),
-    path('salomatlik/<int:pk>', views.SalomatlikDetail.as_view(), name='salomatlikdetail'),
+    path('products/search/', views.SearchViews, name='search-item'),
 ]
