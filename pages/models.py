9e9272a23c991eb5a1abb359b15b12ccff49e519
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-09-25 00:33:15 +0500

    Update models.py and Deleted unnecessary models

--- pages/models.py
+++ pages/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 
-from django.utils.timezone import now
+from django.contrib.auth.models import User
+
+from django.utils import timezone
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='category/')
 
-class ad(models.Model):
+    class Meta:
+        db_table = 'pages_category'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class products(models.Model):
     product_img = models.ImageField(upload_to='images/')
     product_name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
+    category = models.CharField(max_length=90, default='Kitoblar')
     price = models.BigIntegerField()
     phone_number = models.BigIntegerField()
-    post_date = models.DateField(default=now)
+    post_date = models.DateField(default=timezone.now)
     location = models.CharField(max_length=80)
 
     class Meta:
@@ -19,116 +33,3 @@ class ad(models.Model):
 
     def str(self):
         return f'{self.product_name}'
-
-class salomatlik(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
-    phone = models.BigIntegerField()
-    location = models.CharField(max_length=90)
-    price = models.CharField(max_length=90)
-
-    class Meta:
-        db_table = 'salomatlik'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
-class kitoblar(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
-    phone = models.BigIntegerField()
-    location = models.CharField(max_length=90)
-    price = models.CharField(max_length=90)
-
-    class Meta:
-        db_table = 'kitoblar'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
-class quron(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
-    phone = models.BigIntegerField()
-    location = models.CharField(max_length=90)
-    price = models.CharField(max_length=90)
-
-    class Meta:
-        db_table = 'quron'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
-class offis(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
-    phone = models.BigIntegerField()
-    location = models.CharField(max_length=90)
-    price = models.CharField(max_length=90)
-
-    class Meta:
-        db_table = 'offis'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
-class ibodat(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
-    phone = models.BigIntegerField()
-    location = models.CharField(max_length=90)
-    price = models.CharField(max_length=90)
-
-    class Meta:
-        db_table = 'ibodat'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
-class sovga(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
-    phone = models.BigIntegerField()
-    location = models.CharField(max_length=90)
-    price = models.CharField(max_length=90)
-
-    class Meta:
-        db_table = 'sovga'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
-class parfyum(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
-    phone = models.BigIntegerField()
-    location = models.CharField(max_length=90)
-    price = models.CharField(max_length=90)
-
-    class Meta:
-        db_table = 'parfyum'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
-class gozallik(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=255)
-    phone = models.BigIntegerField()
-    location = models.CharField(max_length=90)
-    price = models.CharField(max_length=90)
-
-    class Meta:
-        db_table = 'gozallik'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
