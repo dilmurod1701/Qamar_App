71f1371d9d57f73b5298d8a86ad2ca36e941f374
Sarvar <118446481+OmonovSarvar@users.noreply.github.com>
2023-09-25 20:39:56 +0500

    Update models.py
    Updated Models.py

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class CreateAccount(models.Model):
+    last_name = models.CharField(max_length=55)
+    first_name = models.CharField(max_length=55)
     username = models.CharField(max_length=150)
     email = models.EmailField(max_length=70)
     password1 = models.CharField(max_length=33)
