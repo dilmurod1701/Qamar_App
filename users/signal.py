c778fea1732112ce22467ad091ebfc60e9a17114
Sarvar <118446481+OmonovSarvar@users.noreply.github.com>
2023-09-25 19:14:11 +0500

    Update signal.py
    Updated signal.py

--- users/signal.py
+++ users/signal.py
@@ -3,18 +3,13 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_delete, post_delete
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import CreateAccount
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.crete(user=instance)
+        CreateAccount.objects.crete(user=instance)
         instance.save()
-        subject = 'Assalomu alekum'
-        message = "Tabriklayman bugundan boshlab siz bizning foydalanuvchimiz qatoriga qo'shildingiz"
-        from_mail = 'backendchi.bollar.no1@gmail.com'
-        to = 'absoluteomon999@gmail.com'
-        send_mail(subject=subject, message=message,from_email=from_mail, recipient_list=[to])
 
 @receiver(pre_delete, sender=User)
 def delete_profile(sender, instance, **kwargs):
